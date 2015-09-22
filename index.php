a11f07a4c7cb6864da960a9b8403bcc91bb53f5c
Евгений Васильцов <eugenevasilsov@gmail.com>
2015-09-22 13:24:49 +0300

    fixed category filter. add exeption - if vendor/autoload.php not exist

--- index.php
+++ index.php
@@ -37,7 +37,16 @@ switch (ENVIRONMENT)
 }
 
 
-# Подключаем bootstrap файл
-require_once 'vendor/autoload.php';
+
+/** load composer autoloder */
+if (file_exists('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+} else {
+    echo "<h1>Please install via composer.json</h1>";
+    echo "<p>Install Composer instructions: <a href='https://getcomposer.org/doc/00-intro.md#globally'>https://getcomposer.org/doc/00-intro.md#globally</a></p>";
+    echo "<p>Once composer is installed navigate to the working directory in your terminal/command promt and enter 'composer install'</p>";
+    exit;
+}
+
 
 Route::start();
