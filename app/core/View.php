4fafd3b022e5c68ffbebbe74d5b6c0a03eee2d81
Евгений Васильцов <eugenevasilsov@gmail.com>
2015-10-20 00:25:07 +0300

    fixed View.php

--- app/core/View.php
+++ app/core/View.php
@@ -4,10 +4,11 @@ class View
 {
     public $loader;
     public $twig;
-    public $path = ROOT . '/app/views/skins';
+    public $path;
 
     function __construct()
     {
+        $this->path = ROOT . '/app/views/skins';
         $this->loader = new Twig_Loader_Filesystem($this->path);
         $this->twig = new Twig_Environment($this->loader);
     }
